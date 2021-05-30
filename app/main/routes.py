a6f71911e95525d7593eadf4909500e247af8fb5
Brian Faivre <brian.faivre@gmail.com>
2021-05-30 11:59:54 -0400

    Check if user is auth before querying.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask_login import current_user
 from app import __version__
 from app.main import main
 from app.models import Enterprise, EventFrameGroup
@@ -6,6 +7,11 @@ from app.models import Enterprise, EventFrameGroup
 @main.route("/")
 def index():
 	version = __version__
-	enterprises = Enterprise.query.all()
-	eventFrameGroups = EventFrameGroup.query.all()
+	if current_user.is_authenticated:
+		enterprises = Enterprise.query.all()
+		eventFrameGroups = EventFrameGroup.query.all()
+	else:
+		enterprises = None
+		eventFrameGroups = None
+
 	return render_template("main/index.html", enterprises = enterprises, eventFrameGroups = eventFrameGroups, version = version)
